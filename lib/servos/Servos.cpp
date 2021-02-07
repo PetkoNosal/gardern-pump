88bbdf9159930e8a1d90cb13344dacc2f24ed25f
peter.nosal <petonosal@gmail.com>
2021-02-07 18:55:54 +0100

    Calibrate positions (based on actual servo-valve interaction) and set every servo position independently
    Signed-off-by: peter.nosal <petonosal@gmail.com>

--- lib/servos/Servos.cpp
+++ lib/servos/Servos.cpp
@@ -2,29 +2,59 @@
 #include <Servo.h>
 
 #define PIN_MOTOR 11
-#define PIN_IN_STREAM 10
-#define PIN_IN_BARREL 9
-#define PIN_OUT_BARREL 5
-#define PIN_OUT_HOSE 6
+#define SERVO_DELAY 500
+
+/// SERVO FOR VALVE IN-STREAM ////
+#define IN_STREAM_PIN 10
+#define IN_STREAM_MIN 20
+#define IN_STREAM_MAX 95
+//////////////////////////////////
+
+/// SERVO FOR VALVE IN-BARREL ////
+#define IN_BARREL_PIN 9
+#define IN_BARREL_MIN 20
+#define IN_BARREL_MAX 110
+//////////////////////////////////
+
+/// SERVO FOR VALVE OUT-BARREL ///
+#define OUT_BARREL_PIN 5
+#define OUT_BARREL_MIN 20
+#define OUT_BARREL_MAX 120
+//////////////////////////////////
+
+/// SERVO FOR VALVE OUT-HOSE /////
+#define OUT_HOSE_PIN 6
+#define OUT_HOSE_MIN 20
+#define OUT_HOSE_MAX 100
+//////////////////////////////////
 
 typedef struct {
-    Servo valve_in_stream;
-    Servo valve_in_barrel;
-    Servo valve_out_barrel;
-    Servo valve_out_hose;
+    Servo servo;
+    int pin;
+    int min;
+    int max;
+    bool state;
+} valve_servo_t;
+
+typedef struct servos {
+    valve_servo_t valve_in_stream = {Servo(), IN_STREAM_PIN, IN_STREAM_MIN, IN_STREAM_MAX, true};
+    valve_servo_t valve_in_barrel = {Servo(), IN_BARREL_PIN, IN_BARREL_MIN, IN_BARREL_MAX, true};
+    valve_servo_t valve_out_barrel = {Servo(), OUT_BARREL_PIN, OUT_BARREL_MIN, OUT_BARREL_MAX, true};
+    valve_servo_t valve_out_hose = {Servo(), OUT_HOSE_PIN, OUT_HOSE_MIN, OUT_HOSE_MAX, true};
 } servos_t;
 
 static servos_t servos;
 
 void drivePump(byte _percentage);
-void setServo(Servo &_servo, bool _state);
+void setServo(valve_servo_t &_servo, bool _state);
 
 void Servos::init() {
     pinMode(PIN_MOTOR, OUTPUT);
-    servos.valve_in_stream.attach(PIN_IN_STREAM);
-    servos.valve_in_barrel.attach(PIN_IN_BARREL);
-    servos.valve_out_barrel.attach(PIN_OUT_BARREL);
-    servos.valve_out_hose.attach(PIN_OUT_HOSE);
+
+    servos.valve_in_stream.servo.attach(servos.valve_in_stream.pin);
+    servos.valve_in_barrel.servo.attach(servos.valve_in_barrel.pin);
+    servos.valve_out_barrel.servo.attach(servos.valve_out_barrel.pin);
+    servos.valve_out_hose.servo.attach(servos.valve_out_hose.pin);
 
     drivePump(0);
     setServo(servos.valve_in_stream, false);
@@ -44,11 +74,18 @@ void Servos::actOnChanges(update_t &_update, state_t &_state) {
     }
 }
 
-void setServo(Servo &_servo, bool _state) {
-    if (_state) {
-        _servo.write(120);
-    } else {
-        _servo.write(20);
+void setServo(valve_servo_t &_servo, bool _state) {
+    if (_state != _servo.state) {
+        if (_state) {
+            _servo.servo.write(_servo.max + 15);
+            delay(SERVO_DELAY);
+            _servo.servo.write(_servo.max);
+        } else {
+            _servo.servo.write(_servo.min - 15);
+            delay(SERVO_DELAY);
+            _servo.servo.write(_servo.min);
+        }
+        _servo.state = _state;
     }
 }
 
